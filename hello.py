de971286615cde3041a2f3a62c2c6b67185a00e1
zeezu124 <zeezu124@gmail.com>
2022-05-11 07:22:02 +0100

    A file filled with basic python jitterings

---
+++ hello.py
@@ -0,0 +1,15 @@
+from doctest import Example
+
+
+boy = 67
+girl = 68
+woman = 21
+
+eg_list = [boy, girl, woman, "boy", "girl", "omo"]
+
+print(boy + girl)
+print(woman + 2)
+
+
+for x in eg_list:
+    print(x)
